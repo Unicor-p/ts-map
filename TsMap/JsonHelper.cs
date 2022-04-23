0888054ba883977b908d937722741bb84510dafa
SMITH Emmanuel <contact@jagfx.fr>
2022-04-23 21:09:40 +0200

    :rewind: [TsMap] reset the original project at source

--- TsMap/JsonHelper.cs
+++ TsMap/JsonHelper.cs
@@ -1,34 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Web.Script.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using TsMap.TsTileMapInfo;
 
 namespace TsMap {
     public class JsonHelper {
-        private static readonly string _settingsPath =
-            Path.Combine( Environment.GetFolderPath( Environment.SpecialFolder.LocalApplicationData ), "ts-map" );
+        private static readonly string _settingsPath = Path.Combine( Environment.GetFolderPath( Environment.SpecialFolder.LocalApplicationData ), "ts-map" );
 
-        public static void SaveTileMapInfo( string path,
-                                            string tileMapInfoStructure,
-                                            TsGame game,
-                                            int    mapPadding,
-                                            int    tileSize,
-                                            float  x1,
-                                            float  x2,
-                                            float  y1,
-                                            float  y2,
-                                            int    minZoom,
-                                            int    maxZoom,
-                                            string gameVersion ) {
-            JObject tileMapInfo = new TsTileMapDefault()
-                .TileMapInfo( game, mapPadding, tileSize, x1, x2, y1, y2, minZoom, maxZoom, gameVersion );
-
-            if ( tileMapInfoStructure == TsTileMapJagfxDash.Name )
-                tileMapInfo = new TsTileMapJagfxDash()
-                    .TileMapInfo( game, mapPadding, tileSize, x1, x2, y1, y2, minZoom, maxZoom, gameVersion );
+        public static void SaveTileMapInfo( string path, float x1, float x2, float y1, float y2, int minZoom, int maxZoom ) {
+            var tileMapInfo = new JObject {
+                [ "x1" ]      = x1,
+                [ "x2" ]      = x2,
+                [ "y1" ]      = y1,
+                [ "y2" ]      = y2,
+                [ "minZoom" ] = minZoom,
+                [ "maxZoom" ] = maxZoom
+            };
 
             Directory.CreateDirectory( path );
             File.WriteAllText( Path.Combine( path, "TileMapInfo.json" ), tileMapInfo.ToString( Formatting.Indented ) );
@@ -36,22 +23,12 @@ namespace TsMap {
 
         public static void SaveSettings( Settings settings ) {
             Directory.CreateDirectory( _settingsPath );
-            File.WriteAllText( Path.Combine( _settingsPath, "Settings.json" ),
-                               JsonConvert.SerializeObject( settings, Formatting.Indented ) );
+            File.WriteAllText( Path.Combine( _settingsPath, "Settings.json" ), JsonConvert.SerializeObject( settings, Formatting.Indented ) );
         }
 
         public static Settings LoadSettings() {
             if ( !File.Exists( Path.Combine( _settingsPath, "Settings.json" ) ) ) return new Settings();
-            return JsonConvert.DeserializeObject< Settings >( File.ReadAllText( Path.Combine( _settingsPath,
-                                                                                   "Settings.json" ) ) );
-        }
-
-        public static void SaveRoadPoints( List< dynamic > points ) {
-            Directory.CreateDirectory( _settingsPath );
-            var serializer = new JavaScriptSerializer();
-
-            serializer.MaxJsonLength = int.MaxValue;
-            File.WriteAllText( Path.Combine( _settingsPath, "RoadPoints.json" ), serializer.Serialize( points ) );
+            return JsonConvert.DeserializeObject< Settings >( File.ReadAllText( Path.Combine( _settingsPath, "Settings.json" ) ) );
         }
     }
 }
